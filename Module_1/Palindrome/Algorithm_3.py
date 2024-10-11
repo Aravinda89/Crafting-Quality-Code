691a6ce34727edb60e4f2bbec0ceadc0254c3cb1
aravinda_gn <aravinda.gn123@gmail.com>
2024-10-11 13:32:51 +0530

    Update is_palindrome_v3

--- Module_1/Palindrome/Algorithm_3.py
+++ Module_1/Palindrome/Algorithm_3.py
@@ -10,4 +10,12 @@ def is_palindrome_v3(s):
     >>> is_palindrome_v3('dented')
     False
     """
-    
+
+    i = 0
+    j = len(s) - 1
+
+    while i < j and s[i] == s[j]:
+        i = i + 1
+        j = j - 1
+
+    return j <= i
