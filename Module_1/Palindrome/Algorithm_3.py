1e8dade5eb3b6cd83c8891a2a8140609055809d3
aravinda_gn <aravinda.gn123@gmail.com>
2024-10-11 12:19:51 +0530

    Create Algorithm_3.py

---
+++ Module_1/Palindrome/Algorithm_3.py
@@ -0,0 +1,13 @@
+def is_palindrome_v3(s):
+    """ (str) -> bool
+
+    Return True if and only if s is a palindrome.
+
+    >>> is_palindrome_v3('noon')
+    True
+    >>> is_palindrome_v3('racecar')
+    True
+    >>> is_palindrome_v3('dented')
+    False
+    """
+    
